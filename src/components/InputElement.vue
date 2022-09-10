3c444299dd7946bd97576fa4705371852f600cd8
Maharta <gedemaharta.dev@gmail.com>
2022-09-11 06:18:40 +0800

    moved the components class to computed property and used composition API

--- src/components/InputElement.vue
+++ src/components/InputElement.vue
@@ -2,19 +2,26 @@
   <input
     :type="type"
     :id="id"
+    :mode="mode"
     :value="modelValue"
     @input="updateInput"
-    class="rounded-sm outline outline-1 focus:outline-2 outline-slate-500 mb-4 block p-3"
+    :class="inputElementClass"
   />
 </template>
 
-<script>
-export default {
-  props: ["type", "id", "modelValue"],
-  methods: {
-    updateInput(event) {
-      this.$emit("update:modelValue", event.target.value);
-    },
-  },
-};
+<script setup>
+import { defineProps, defineEmits, computed } from "vue";
+
+const props = defineProps(["type", "id", "mode", "modelValue"]);
+const emit = defineEmits("update:modelValue");
+
+const inputElementClass = computed(() => {
+  return props.mode === "small"
+    ? "rounded-sm outline outline-1 focus:outline-2 outline-slate-500 mb-4 block"
+    : "rounded-sm outline outline-1 focus:outline-2 outline-slate-500 mb-4 block p-3";
+});
+
+function updateInput(event) {
+  emit("update:modelValue", event.target.value);
+}
 </script>
