31b2c0f0adaaf6a9cbb56a36fd0229cd23d2573a
Maharta <gedemaharta.dev@gmail.com>
2022-09-14 15:31:35 +0800

    Now uses loading state and empty state handler. Now also use UseListenToDataChanges composable. added function to move data to pending status, where "ekspedisi" role can confirm the pending data.

--- src/views/OrderPage.vue
+++ src/views/OrderPage.vue
@@ -1,24 +1,41 @@
 <template>
-  <DataTable :value="documentList" :paginator="true" :rows="10">
+  <DataTable
+    :value="documentList"
+    :paginator="true"
+    :rows="10"
+    :loading="isLoading"
+  >
+    <template #empty> No records found </template>
+    <template #loading> Loading Data..</template>
+
     <Column field="id" :sortable="true" header="ID"></Column>
     <Column field="nilai" header="Nilai" :sortable="true"> </Column>
     <Column field="tanggal" header="Tanggal" :sortable="true"></Column>
     <Column field="keterangan" header="Keterangan" :sortable="true"></Column>
+    <Column field="owner" header="Owner" :sortable="true"></Column>
+    <Column field="status" header="Status" :sortable="true"></Column>
     <Column headerStyle="width: 8em" bodyStyle="text-align: center">
       <template #header>
         <Button type="button" icon="pi pi-cog"></Button>
       </template>
-      <template #body>
+      <template #body="slotProps">
         <Button
           type="button"
-          icon="pi pi-search"
+          icon="pi pi-check-circle"
           class="p-button-success"
           style="margin-right: 0.5em"
+          @click="moveToPending(slotProps.data)"
         ></Button>
         <Button
           type="button"
-          icon="pi pi-pencil"
+          icon="pi pi-trash"
           class="p-button-warning"
+          @click="
+            deleteDocument(
+              slotProps.data.id,
+              OwnerTranslator[userStore.user.uid]
+            )
+          "
         ></Button>
       </template>
     </Column>
@@ -26,39 +43,40 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { collection, addDoc, Timestamp } from "@firebase/firestore";
+import { useListenToDataChanges } from "../composables/useListenToDataChanges";
+import { db } from "../firebase/config";
 import { useUserStore } from "../stores/userStore";
-import { db } from "../firebase/config.js";
-import { collection, onSnapshot } from "firebase/firestore";
-import { OwnerTranslator } from "../firebase/firestoreHelper";
-import { onBeforeRouteLeave } from "vue-router";
+import {
+  OwnerTranslator,
+  deleteDocument,
+  Document,
+  DocumentConverter,
+} from "../firebase/firestoreHelper";
 
-const documentList = ref([]);
 const userStore = useUserStore();
-let unsub;
+const collectionName = OwnerTranslator[userStore.user.uid];
+const collectionRef = collection(db, collectionName);
+const { documentList, isLoading } = useListenToDataChanges(collectionRef);
 
-if (userStore.user) {
+async function moveToPending(data) {
+  const collectionName = OwnerTranslator[userStore.user.uid];
+  deleteDocument(data.id, collectionName);
   try {
-    const collectionName = OwnerTranslator[`${userStore.user.uid}`];
-    const collectionRef = collection(db, collectionName);
-    unsub = onSnapshot(collectionRef, (col) => {
-      documentList.value = [];
-
-      col.docs.forEach((doc) => {
-        const data = doc.data();
-        const convertedTimestamp = {
-          tanggal: data.tanggal.toDate().toLocaleString(),
-        };
-        Object.assign(data, convertedTimestamp);
-        documentList.value.push(data);
-      });
+    const document = new Document({
+      id: data.id,
+      nilai: data.nilai,
+      ket: data.keterangan,
+      owner: data.owner,
+      tanggal: Timestamp.fromMillis(Date.parse(data.tanggal)),
     });
+    const docRef = await addDoc(
+      collection(db, "pending"),
+      DocumentConverter.toFireStore(document)
+    );
+    console.log(`document added with an id of ${docRef.id}`);
   } catch (error) {
-    alert(error.message);
+    console.log(error.message);
   }
 }
-
-onBeforeRouteLeave(() => {
-  unsub();
-});
 </script>
