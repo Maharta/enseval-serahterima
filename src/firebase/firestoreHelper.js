949962f55e8b0a74ea2ac4e31dde60a7b2d9467d
Maharta <gedemaharta.dev@gmail.com>
2022-09-14 15:28:06 +0800

    adjusted Document class properties and added deleteDoc helper function

--- src/firebase/firestoreHelper.js
+++ src/firebase/firestoreHelper.js
@@ -1,7 +1,19 @@
-import { Timestamp } from "firebase/firestore";
-import { auth } from "../firebase/config";
+import {
+  collection,
+  deleteDoc,
+  getDocs,
+  query,
+  Timestamp,
+  where,
+} from "firebase/firestore";
+import { db } from "./config";
 
 const EKPEDISI_ID = "RWyBwDwiVtWa9s6FPfszzNFcDth2";
+
+const isEkspedisi = (id) => {
+  if (id === EKPEDISI_ID) return true;
+  return false;
+};
 
 const OwnerTranslator = {
   qKISUN5y6vfuq7W8XQDhLX71Qzk2: "farma",
@@ -9,22 +21,25 @@ const OwnerTranslator = {
 };
 
 class Document {
-  constructor({ id, nilai, tanggal = Timestamp.now(), ket }) {
+  constructor({ id, nilai, tanggal = Timestamp.now(), ket, owner }) {
     this.id = id;
     this.nilai = nilai;
     this.tanggal = tanggal;
     this.keterangan = ket;
+    this.owner = owner;
+    this.status = "pending";
   }
 }
 
 const DocumentConverter = {
   toFireStore: (document) => {
-    console.log(auth.currentUser.uid);
     return {
       id: document.id,
       nilai: document.nilai,
       tanggal: document.tanggal,
       keterangan: document.keterangan,
+      owner: document.owner,
+      status: document.status,
     };
   },
   fromFirestore: (snapshot, options) => {
@@ -33,4 +48,19 @@ const DocumentConverter = {
   },
 };
 
-export { Document, DocumentConverter, EKPEDISI_ID, OwnerTranslator };
+const deleteDocument = async (id, collectionName) => {
+  const q = query(collection(db, collectionName), where("id", "==", id));
+  const querySnapshot = await getDocs(q);
+  querySnapshot.forEach((doc) => {
+    deleteDoc(doc.ref);
+  });
+};
+
+export {
+  Document,
+  DocumentConverter,
+  EKPEDISI_ID,
+  OwnerTranslator,
+  isEkspedisi,
+  deleteDocument,
+};
