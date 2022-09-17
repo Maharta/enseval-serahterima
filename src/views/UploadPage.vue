52908c9e9bbed7ed32bdba3a9069f9855e50143f
Maharta <gedemaharta.dev@gmail.com>
2022-09-17 10:10:22 +0800

    refactored code, using reactive and new inputelement

--- src/views/UploadPage.vue
+++ src/views/UploadPage.vue
@@ -3,20 +3,35 @@
     <div>
       <form
         @submit.prevent="uploadData"
-        class="flex flex-col items-center justify-center"
+        class="flex flex-col items-center justify-center card"
       >
         <div>
-          <label for="id">ID Dokumen</label>
-          <InputElement type="text" mode="small" id="id" v-model="id" />
+          <InputElement
+            label="ID Dokumen"
+            type="text"
+            mode="small"
+            id="id"
+            v-model="form.id"
+          />
         </div>
 
         <div>
-          <label for="nilai">Nilai Dokumen</label>
-          <InputElement type="text" mode="small" id="nilai" v-model="nilai" />
+          <InputElement
+            label="Nilai Dokumen"
+            type="text"
+            mode="small"
+            id="id"
+            v-model="form.nilai"
+          />
         </div>
         <div>
-          <label for="ket">Keterangan</label>
-          <InputElement type="text" mode="small" id="ket" v-model="ket" />
+          <InputElement
+            label="Keterangan"
+            type="text"
+            mode="small"
+            id="id"
+            v-model="form.ket"
+          />
         </div>
         <div>
           <button>Upload Order</button>
@@ -28,7 +43,7 @@
 
 <script setup>
 import InputElement from "../components/InputElement.vue";
-import { ref } from "vue";
+import { reactive } from "vue";
 import { collection, addDoc } from "firebase/firestore";
 import { db } from "../firebase/config";
 import {
@@ -38,16 +53,19 @@ import {
 } from "../firebase/firestoreHelper";
 import { useUserStore } from "../stores/userStore";
 
-const id = ref("");
-const nilai = ref("");
-const ket = ref("");
+const form = reactive({
+  id: "",
+  nilai: "",
+  ket: "",
+});
+
 const userStore = useUserStore();
 
 async function uploadData() {
   const dokumen = new Document({
-    id: id.value,
-    nilai: nilai.value,
-    ket: ket.value,
+    id: form.id,
+    nilai: form.nilai,
+    ket: form.ket,
     owner: OwnerTranslator[`${userStore.user.uid}`],
   });
 
