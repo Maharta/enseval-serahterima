a233f118a78e77ffb2a0778bca450883b76d282b
Maharta <gedemaharta.dev@gmail.com>
2022-09-30 15:35:01 +0800

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { splitVendorChunkPlugin } from "vite";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), splitVendorChunkPlugin()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
