b68911dbcc18be2c6c67f313a10ae014a00d2129
Maharta <gedemaharta.dev@gmail.com>
2022-09-30 15:41:12 +0800

    added v-if to hide buttons from ekspedition role

--- src/components/layout/LeftNavigation.vue
+++ src/components/layout/LeftNavigation.vue
@@ -2,10 +2,10 @@
   <nav
     class="flex justify-around text-slate-500 border-r border-r-black p-2 md:inline-block md:h-full md:w-36 md:text-center md:absolute"
   >
-    <div class="md:my-5 hover:text-black">
+    <div v-if="!isEkspedisi" class="md:my-5 hover:text-black">
       <router-link to="/upload" href="">Upload</router-link>
     </div>
-    <div class="md:my-5 hover:text-black">
+    <div v-if="!isEkspedisi" class="md:my-5 hover:text-black">
       <router-link to="/data">Data</router-link>
     </div>
     <div class="md:my-5 hover:text-black">
@@ -19,3 +19,12 @@
     </div>
   </nav>
 </template>
+
+<script setup>
+import { ref } from "vue";
+import { OwnerTranslator } from "../../firebase/firestoreHelper";
+import { useUserStore } from "../../stores/userStore";
+
+const userStore = useUserStore();
+const isEkspedisi = ref(OwnerTranslator[userStore.user.uid] === "ekspedisi");
+</script>
