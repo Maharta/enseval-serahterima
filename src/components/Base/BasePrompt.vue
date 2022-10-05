a6725dc24b8800859f392908a49483d5d9a8c553
Maharta <gedemaharta.dev@gmail.com>
2022-10-05 14:52:44 +0800

    added a BasePrompt component

---
+++ src/components/Base/BasePrompt.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="prompt">
+    <div class="body"><slot name="body"></slot></div>
+    <div class="actions">
+      <slot name="actions">
+        <Button
+          class="p-button-success"
+          icon="pi pi-check-circle"
+          @click="emit('confirm')"
+        />
+      </slot>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const emit = defineEmits(["confirm", "cancel"]);
+</script>
+
+<style scoped>
+.prompt {
+  padding: 1rem;
+  padding-right: 0;
+  padding-bottom: 0;
+}
+
+.prompt div {
+  margin-bottom: 8px;
+}
+
+.actions {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
