056cd3067a2a3de5f284589c30586507ff9e3b8a
Maharta <gedemaharta.dev@gmail.com>
2022-10-06 14:36:42 +0800

    refactored orderForm component to be more reusable

--- src/components/forms/OrderForm.vue
+++ src/components/forms/OrderForm.vue
@@ -1,6 +1,6 @@
 <template>
   <form
-    @submit.prevent="editOrder"
+    @submit.prevent="emitSubmit"
     class="flex flex-col items-center justify-center"
   >
     <div>
@@ -12,6 +12,8 @@
         :value="idState.input.value"
         @blur="idProps.onBlur"
         @input="idProps.onInput"
+        :class="idInputClass"
+        :placeholder="idProps.placeHolder.value"
       />
     </div>
 
@@ -24,6 +26,8 @@
         :value="nilaiState.input.value"
         @blur="nilaiProps.onBlur"
         @input="nilaiProps.onInput"
+        :class="nilaiInputClass"
+        :placeholder="nilaiProps.placeHolder.value"
       />
     </div>
     <div>
@@ -35,6 +39,8 @@
         :value="ketState.input.value"
         @blur="ketProps.onBlur"
         @input="ketProps.onInput"
+        :class="ketInputClass"
+        :placeholder="ketProps.placeHolder.value"
       />
     </div>
     <Button :disabled="!isFormValid" type="submit">{{ btnLabel }}</Button>
@@ -43,36 +49,62 @@
 
 <script setup>
 import InputElement from "../forms/InputElement.vue";
-import { editDocument, OwnerTranslator } from "../../firebase/firestoreHelper";
-import { useUserStore } from "../../stores/userStore";
 import { useInput } from "../../composables/useInput";
 import { validateNoEmpty } from "../../helpers/validateHelpers";
-import { useModalStore } from "../../stores/modalStore";
 import { computed } from "vue";
-
-const userStore = useUserStore();
-const modalStore = useModalStore();
 
 const props = defineProps({
   btnLabel: String,
-  order: Object,
+  id: {
+    type: String,
+    default: "",
+  },
+  nilai: {
+    type: String,
+    default: "",
+  },
+  keterangan: {
+    type: String,
+    default: "",
+  },
 });
 
-const [idState, idProps] = useInput(validateNoEmpty, props.order.id);
-const [nilaiState, nilaiProps] = useInput(validateNoEmpty, props.order.nilai);
-const [ketState, ketProps] = useInput(validateNoEmpty, props.order.keterangan);
+const emit = defineEmits(["submit"]);
+
+const [idState, idProps] = useInput(validateNoEmpty, props.id);
+const [nilaiState, nilaiProps] = useInput(validateNoEmpty, props.nilai);
+const [ketState, ketProps] = useInput(validateNoEmpty, props.keterangan);
 const isFormValid = computed(
   () =>
     idState.isValid.value && nilaiState.isValid.value && ketState.isValid.value
 );
 
-function editOrder() {
-  const collectionName = OwnerTranslator[userStore.user.uid];
-  editDocument(props.order.id, collectionName, {
-    id: idState.input.value,
-    nilai: nilaiState.input.value,
-    keterangan: ketState.input.value,
-  });
-  modalStore.closeModal();
+const idInputClass = computed(() => {
+  return idState.isInputInvalid.value
+    ? "bg-[#fddddd] border-solid border-[#b40e0e]"
+    : "";
+});
+
+const nilaiInputClass = computed(() => {
+  return nilaiState.isInputInvalid.value
+    ? "bg-[#fddddd] border-solid border-[#b40e0e]"
+    : "";
+});
+
+const ketInputClass = computed(() => {
+  return ketState.isInputInvalid.value
+    ? "bg-[#fddddd] border-solid border-[#b40e0e]"
+    : "";
+});
+// due to tailwind constraint, can't pass arbitrary values from useInput composables since it won't be compiled
+
+function emitSubmit() {
+  emit(
+    "submit",
+    props.id,
+    idState.input.value,
+    nilaiState.input.value,
+    ketState.input.value
+  );
 }
 </script>
