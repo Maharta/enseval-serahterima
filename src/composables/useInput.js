d84112bc6efbb4dfb59b5bd20088e4a401d84f0e
Maharta <gedemaharta.dev@gmail.com>
2022-10-06 13:44:42 +0800

    added useInput composables for better input validation

---
+++ src/composables/useInput.js
@@ -0,0 +1,36 @@
+import { computed, ref } from "vue";
+
+export function useInput(validateFn, initialValue = "") {
+  const input = ref(initialValue);
+  const isTouched = ref(false);
+
+  const isValid = computed(() => {
+    return validateFn(input.value);
+  });
+
+  const isInputInvalid = computed(() => {
+    return !isValid.value && isTouched.value;
+  });
+
+  const placeHolder = computed(() => {
+    return isInputInvalid.value ? "Input tidak boleh kosong!" : "";
+  });
+
+  const onInput = (event) => {
+    input.value = event.target.value;
+  };
+
+  const onBlur = () => {
+    isTouched.value = true;
+  };
+
+  const reset = () => {
+    isTouched.value = false;
+    input.value = "";
+  };
+
+  return [
+    { isTouched, isValid, isInputInvalid, input },
+    { onBlur, onInput, reset, placeHolder },
+  ];
+}
