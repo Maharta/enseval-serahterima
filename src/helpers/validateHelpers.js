8ba47ce258b89853f8045aff412e9f145d22d0c6
Maharta <gedemaharta.dev@gmail.com>
2022-10-26 23:42:35 +0800

    added two validate helpers function

--- src/helpers/validateHelpers.js
+++ src/helpers/validateHelpers.js
@@ -1,3 +1,14 @@
+import { isRef } from "vue";
+
 export function validateNoEmpty(data) {
   return data.trim() !== "";
+}
+
+export function validatePassword(password) {
+  return password.trim() !== "" && password.length >= 6;
+}
+
+export function validateConfirmPassword(newPassword, confirmPassword) {
+  if (isRef(newPassword)) return confirmPassword === newPassword.value;
+  return confirmPassword === newPassword;
 }
